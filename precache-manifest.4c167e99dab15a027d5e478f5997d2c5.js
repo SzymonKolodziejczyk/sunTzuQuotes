c5587292121cfbe67683c988f18a7e913b100597
Szymon Kołodziejczyk <szymonnkolodziejczyk@gmail.com>
2021-05-15 12:46:04 +0200

    Updates

---
+++ precache-manifest.4c167e99dab15a027d5e478f5997d2c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac76d1c4b0b0522f073c58e9e963fb02",
+    "url": "/sunTzuQuotes/index.html"
+  },
+  {
+    "revision": "a8b3946e35a71144bd68",
+    "url": "/sunTzuQuotes/static/css/main.dd3aad60.chunk.css"
+  },
+  {
+    "revision": "5e3437a791c44b8f2d0e",
+    "url": "/sunTzuQuotes/static/js/2.891dcf78.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/sunTzuQuotes/static/js/2.891dcf78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b3946e35a71144bd68",
+    "url": "/sunTzuQuotes/static/js/main.1753b7f4.chunk.js"
+  },
+  {
+    "revision": "0b9463951a93e4d27b69",
+    "url": "/sunTzuQuotes/static/js/runtime-main.5db0bea4.js"
+  },
+  {
+    "revision": "ee2977f0602870454a2f4b7d0020f582",
+    "url": "/sunTzuQuotes/static/media/sunTzu.ee2977f0.png"
+  },
+  {
+    "revision": "638d62d64779723a094f3de19bebdddd",
+    "url": "/sunTzuQuotes/static/media/sunTzuIcon.638d62d6.png"
+  }
+]);
